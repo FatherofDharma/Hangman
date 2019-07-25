27d70ecba2e65616ec0d350454b1a00414642fc1
Benjamin Martinson and Daniel Hongel <daniel@>
2019-07-25 15:54:07 -0700

    add code to make game functional and display wrong guesses as well as guesses remaining.

--- app.rb
+++ app.rb
@@ -1,23 +1,32 @@
 require('sinatra')
 require('sinatra/reloader')
-require('./lib/album')
-require('./lib/song')
 require('pry')
-require 'hangman'
+require './lib/hangman'
 also_reload('lib/**/*.rb')
 also_reload('app.rb')
 
 
 get ('/') do
-  @game = Hangman.new('turtle')
+  game = Hangman.new('turtle')
+  @word = game.word
+  @guesses_left = game.guesses_left
+  @guessed_letters = game.guessed_letters
+  @game_continue = true
+  @spots_to_fill = game.spots_to_fill
   erb(:play_game)
 end
 
 post ('/make-guess') do
-  letter = params(:letter_choice)
-  game = Hangman.current_hangman
-  @game_continue = game.make_guess
+  letter = params[:letter_choice]
+  game = Hangman.current
+  @what = false
   @word = game.word
   @spots_to_fill = game.check_word(letter)
+  @guesses_left = game.guesses_left
+  @guessed_letters = game.guessed_letters
   erb(:play_game)
+end
+
+get ('/next_turn') do
+
 end
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,12 +4,13 @@ require('pry')
 class Hangman
   @@current_hangman = nil
 
-  attr_reader :spots_to_fill, :word
+  attr_reader :spots_to_fill, :word, :guesses_left, :guessed_letters
 
   def initialize(word)
     @word = word
-    @guessesLeft = 6
+    @guesses_left = 6
     @spots_to_fill = [0] * word.length
+    @guessed_letters = []
     @@current_hangman = self
   end
 
@@ -17,16 +18,20 @@ class Hangman
     @@current_hangman
   end
 
-  def make_guess
-    @guessesLeft -= 1
-    guessesLeft
-  end
-
-  def checkWord(letter_choice)
+  def check_word(letter_choice)
+    if letter_choice == ""
+      return @spots_to_fill
+    end
+    found_match = false
     @word.split("").each_with_index do |letter, idx|
       if(letter_choice == letter)
         @spots_to_fill[idx] = 1
+        found_match = true
       end
+    end
+    if !found_match && !@guessed_letters.include?(letter_choice) 
+      @guesses_left -= 1
+      @guessed_letters.push(letter_choice)
     end
     @spots_to_fill
   end
